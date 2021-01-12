9d64d88d6931a4419255fa87358501823a093825
Yimura <andreas.maerten@scarlet.be>
2021-01-12 17:00:39 +0100

    feat(Scripts): Added central point for api's

---
+++ script/api/API.js
@@ -0,0 +1,43 @@
+import { ApiHost } from '../util/Constants.js'
+import Data from '../util/Data.js'
+import Auth from './Auth.js'
+/*
+import Locations from './Locations.js'
+import Measurements from './Measurements.js'
+import Settings from './Settings.js'
+import Users from './Users.js'
+*/
+
+export default class API {
+    auth = new Auth(this);
+    /*
+    locations = new Locations(this);
+    measurements = new Measurements(this);
+    settings = new Settings(this);
+    users = new Users(this);
+    */
+
+    constructor(app) {
+        this.app = app;
+
+        Object.assign(this, Data);
+    }
+
+    /**
+     * @returns {string}
+     */
+    get host() {
+        return ApiHost;
+    }
+
+    getSession() {
+        return sessionStorage.getItem('session');
+    }
+
+    /**
+     * @param {string} sessionId 
+     */
+    setSession(sessionId) {
+        sessionStorage.setItem('session', sessionId);
+    }
+}
