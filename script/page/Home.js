784cc38c002073979fbbedbb9a746ea0cb2d89ed
Yimura <andreas.maerten@scarlet.be>
2021-01-14 19:01:55 +0100

    wip(Home): Dynamic nav items

--- script/page/Home.js
+++ script/page/Home.js
@@ -1,16 +1,42 @@
 export default class Home {
+    _active = 'home';
+
     constructor(app) {
         this.app = app;
     }
+
+    get active() {
+        return this._active;
+    }
     
     domLookUp() {
-        const btnLogout = document.querySelector('.js-btn-logout');
-        btnLogout.addEventListener('click', this.logout.bind(this));
+        const navItems = document.querySelectorAll('.c-main-nav__list .c-main-nav__item');
+        navItems.forEach(navItem => 
+            navItem.addEventListener(
+                'click',
+                this.navItemClicked.bind(this)
+            )
+        );
+    }
+
+    navItemClicked(e) {
+        const el = e.currentTarget;
+
+        if (this.active == el.dataset.pageType) return;
+
+        this._active = el.dataset.pageType;
+
+        switch (this._active) {
+            case 'logout':
+                return this.logout();
+        
+            case 'home':
+                 
+                break;
+        }
     }
 
     logout(e) {
-        e.preventDefault();
-
         this.app.api.auth.revoke();
         this.app.router.navigate('login');
     }
