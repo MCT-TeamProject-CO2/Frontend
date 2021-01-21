439602b08c0b9a59dcca5776c4a0a3cf46d86575
Yimura <andreas.maerten@scarlet.be>
2021-01-21 18:02:41 +0100

    feat(Constants): Added permission levels

--- script/util/Constants.js
+++ script/util/Constants.js
@@ -1,1 +1,3 @@
-export const ApiHost = 'https://teamproject.pieceof.art'
+export const ApiHost = 'https://teamproject.pieceof.art';
+
+export const PermissionLevels = Object.freeze([ 'info', 'admin', 'root' ]);
