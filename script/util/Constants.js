4018a12d3a8b612b86777ce9d388a4789d34de3a
Yimura <andreas.maerten@scarlet.be>
2021-01-26 16:35:49 +0100

    feat(Constants): Added TimeTypes

--- script/util/Constants.js
+++ script/util/Constants.js
@@ -1,3 +1,30 @@
 export const ApiHost = 'https://teamproject.pieceof.art';
 
 export const PermissionLevels = Object.freeze([ 'info', 'admin', 'root' ]);
+
+export const TimeTypes = [
+    {
+        aggregate: '2m',
+        delta: 60
+    },
+    {
+        aggregate: '30m',
+        delta: 1440
+    },
+    {
+        aggregate: '4h',
+        delta: 10080
+    },
+    {
+        aggregate: '15h',
+        delta: 43800
+    },
+    {
+        aggregate: '91h',
+        delta: 262800
+    },
+    {
+        aggregate: '8d',
+        delta: 525601
+    }
+];
