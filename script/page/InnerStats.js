05c8b90a40b99b9274da0b3ed295d1c9e9b957f2
Yimura <andreas.maerten@scarlet.be>
2021-01-26 12:31:07 +0100

    feat(Stats):  Added visualisation

--- script/page/InnerStats.js
+++ script/page/InnerStats.js
@@ -16,23 +16,12 @@ export default class InnerStats {
         };
     }
 
-    getData() {
-        //possible req
-        // endpoint = `http://localhost:8085/api/v1/data?room=BCE.A.1.000&datatype=Humidity`;
-        // fetch(endpoint, {
-        //         method: "GET",
-        //         mode: "cors"
-        //     })
-        //     .then(res => res.json())
-        //     .then(data => console.log(data));
+    async getData(tagString = this.rooms[Math.floor(Math.random() * this.rooms.length)]) {
+        if (!tagString) return this.graph_wrapper.innerHTML = '<p>Please select a room</p>';
 
-        //testing data
-
-        const data = {
-            Humidity: [34, 33, 33, 32, 34, 35, 32, 34, 34, 33, 34, 33, 35, 33, 33, 33, 34, 34, 34, 34, 34, 35, 33, 32, 33, 33, 35, 34, 34, 32, 33, 33, 33, 32, 35, 33, 34, 34, 34, 34]
-        };
+        const data = await this.app.api.measurements.getDelta(tagString, 10, ['humidity']);
     
-        this.renderChart("Humidity", data);
+        this.renderChart("Humidity", data[tagString]);
     }
 
     async getRooms(q = '') {
@@ -42,6 +31,8 @@ export default class InnerStats {
         results.forEach(result => {
             this.datalist.innerHTML += '<option value="'+ result + '"/>';
         });
+
+        return results;
     }
 
     onTimeTypeChange(e) {
@@ -59,94 +50,42 @@ export default class InnerStats {
         }
     }
 
-    renderChart(dataType, data) {
-        this.graph_wrapper.innerHTML = `<canvas class="c-chart js-graph-canvas" data-type="${dataType}"></canvas>`;
+    renderChart(label, data) {
+        if (!data) return this.graph_wrapper.innerHTML = 'There\'s no data available for this room.';
 
-        const colors = [
-            "#7350C7",
-            "#524EDE",
-            "#4A6BD4",
-            "#A04EDE",
-            "#BE4AD4",
-            "#DE1BCE",
-            "#A919D4",
-            "#AA19FA",
-            "#00FFFF",
-            "#FF00FF",
-            "#9400D3",
-            "#1E90FF",
-            "#7CFC00",
-            "#00FF7F",
-            "#CE27A4",
-            "#E8217B",
-            "#FDE74C",
-            "#5BC0EB",
-            "#EA4848"
-        ];
+        this.graph_wrapper.innerHTML = `<canvas class="c-chart js-graph-canvas"></canvas>`;
 
-        if (data.hasOwnProperty(dataType)) {
-            const chart = document.querySelector(".js-graph-canvas").getContext("2d");
-            // let gradientStroke = chart.createLinearGradient(0, 0, 0, double(chart.height));
-            // gradientStroke.addColorStop(0, colors[Math.floor(Math.random() * colors.length)]);
-            // gradientStroke.addColorStop(0.25, colors[Math.floor(Math.random() * colors.length)]);
-            // gradientStroke.addColorStop(0.5, colors[Math.floor(Math.random() * colors.length)]);
-            // gradientStroke.addColorStop(0.75, colors[Math.floor(Math.random() * colors.length)]);
-            // gradientStroke.addColorStop(1, colors[Math.floor(Math.random() * colors.length)]);
-            new Chart(chart, {
-                type: 'line',
-                data: {
-                    labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    datasets: [{
-                        label: "x-tag",
-                        data: data[dataType],
-                        pointBackgroundColor: "#44c8f5",
-                        pointBorderWidth: 1,
-                        fill: 0,
-                        borderColor: "#44c8f5",
-                    }]
-                },
-                options: {
-                    title: {
-                        display: true,
-                        text: "y-tag",
-                        fontColor: "#334DCC",
-                        fontSize: 20,
-                        padding: 8
-                    },
-                    legend: {
-                        display: false
-                    },
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                fontFamily: "trade-gothic-next",
-                                fontColor: "#334DCC",
-                                fontStyle: "400",
-                                fontSize: 14
-                            }
-                        }],
-                        xAxes: [{
-                            ticks: {
-                                fontFamily: "trade-gothic-next",
-                                fontColor: "#334DCC",
-                                fontStyle: "400",
-                                fontSize: 14
-                            }
-                        }]
-                    }
-                }
-            });
-        }
+        const labels = [];
+        const dataPoints = [];
+
+        data.forEach(dataPoint => {
+            labels.push(dataPoint._time);
+            
+            dataPoints.push(dataPoint._value);
+        });
+
+        const ctx = document.querySelector('.js-graph-canvas');
+        this.chart = new Chart(ctx, {
+            type: 'line',
+
+            data: {
+                labels,
+                datasets: [{
+                    label,
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: dataPoints
+                }]
+            }
+        });
+
+        // Open Sans
     }
 
-    run() {
+    async run() {
         this.domLookup();
 
-        this.getRooms();
+        this.rooms = await this.getRooms();
         this.getData();
     }
 }
